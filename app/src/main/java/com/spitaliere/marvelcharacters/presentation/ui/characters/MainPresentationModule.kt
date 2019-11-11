ec62aabf48f1601e2ec9668b8ea9abfa66b36911
Rafael Spitaliere <rafael.spitaliere@gmail.com>
2019-11-11 00:43:15 -0300

    koin fix

--- app/src/main/java/com/spitaliere/marvelcharacters/presentation/ui/characters/MainPresentationModule.kt
+++ app/src/main/java/com/spitaliere/marvelcharacters/presentation/ui/characters/MainPresentationModule.kt
@@ -3,7 +3,6 @@ package com.spitaliere.marvelcharacters.presentation.ui.characters
 import com.spitaliere.marvelcharacters.presentation.ui.characters.fragments.details.ComicsAdapter
 import com.spitaliere.marvelcharacters.presentation.ui.characters.fragments.heroes.HeroesPagedAdapter
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 /**
@@ -13,10 +12,8 @@ object MainPresentationModule {
 
     fun getModule() = module {
         viewModel { MainViewModel(dataSourceFactory = get(), retryUseCase = get())  }
-        scope(named<MainActivity>()) {
-            factory { HeroesPagedAdapter() }
-            factory { ComicsAdapter() }
-        }
+        factory { HeroesPagedAdapter() }
+        factory { ComicsAdapter() }
     }
 
 }
